1a3bbe415197864d0a12a64f814a0ccf7973b6aa
blc132 <pawelkobrynski97@gmail.com>
2019-04-05 17:19:33 +0200

    added home controller and service

---
+++ TextComparer.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TextComparer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        // GET api/values
+        [HttpPost("{textPattern, textsToCompare, splitText}")]
+        public string CompareText(string textPattern, string textsToCompare, string splitTexts)
+        {
+            return textPattern + " " + textsToCompare + " " + splitTexts;
+        }
+    }
+}
---
+++ TextComparer.Api/Services/HomeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TextComparer.Api.Services.Interfaces;
+
+namespace TextComparer.Api.Services
+{
+    public class HomeService : IHomeService
+    {
+        IList<string> SplitTexts
+    }
+}
